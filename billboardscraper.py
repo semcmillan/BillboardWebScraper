c36c23af1ba2cd078b661836776c1d26063f2ea9
Seth McMillan <semcmill@ncsu.edu>
2018-03-23 09:59:32 -0400

    Restructured files

--- billboardscraper.py
+++ billboardscraper.py
@@ -10,16 +10,16 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from datetime import timedelta
 import numpy
 
+# It will be necessary to create a credentialfiles.py file, which has a class Credentials. This class should only contain
+#  ClientID, ClientSecret, RedirectURI, UserID, Username, and Scope. Refer to readme for more information, and spotipy documentation 
+#  on authentication
+from credentialfiles import Credentials
+
 songList = []
 sp = ''
-ClientID 		= 'cdf75d8320f640d09cc179c34d18e803'
-ClientSecret 	= 'f9be99648e40467f8c52a3bc7ef2d65a'
-RedirectURI 	= 'http://localhost:/callback'
-UserID 			= '1224319843'
-username		= 'semcmill'
-scope 			= 'user-library-read'
 
-# Class which is used to track song information
+# Class which is used to track song information, can additionally be used to print information about
+#  the class
 class SongIndex:
 	def __init__(self, SongTitle, ArtistName, Year, Month, Day, Rank):
 		self.songtitle = SongTitle
@@ -49,6 +49,7 @@ class SongIndex:
 
 
 # Webscrapes from the billboard hot 100 list, to get the specific list for a year, date, and month
+#  Takes a date in the datetime format, which it adds to a URL string to get the request URL
 def scrape_billboard(date):
 	global songList
 	date = str(date)
@@ -71,14 +72,19 @@ def scrape_billboard(date):
 		songList.append(SongInstance)
 
 
-def loop_back_through_data():
+# This function calls the scrape_billboard() function in a loop, with decreasing weeks. Essentially, this will allow you to change 
+#  how many weeks of data you are collecting. The NumWeeks value is an integer which allows you specify that number of weeks
+def loop_back_through_data(NumWeeks):
 	global songList
-	for i in range(0,3):
+	for i in range(0, NumWeeks):
 		d = datetime.date(2017, 9, 30) - timedelta(days=7*i)
 		scrape_billboard(d)
 
 
 # Uses a simple url search to splice the uri of the track, on spotify. This will be used later to search through spotify. 
+#  Although not the most effecient way, this is a safe and easy way to bypass spotify's x requests a second, and can be done much faster,
+#  assuming a stable internet connection. If the song is not found to have an URI (if no links include spotify.com/track/), then an 
+#  'N' is inserted instead.
 def get_song_URI():
 	global songList
 	try:
@@ -115,7 +121,7 @@ def get_spotify_details():
 
 def initialize_spotify():
 	global username, scope, ClientID, ClientSecret, RedirectURI, sp
-	token = util.prompt_for_user_token(username, scope, client_id = ClientID, client_secret = ClientSecret, redirect_uri = RedirectURI)
+	token = util.prompt_for_user_token(Credentials.username, Credentials.scope, client_id = Credentials.ClientID, client_secret = Credentials.ClientSecret, redirect_uri = Credentials.RedirectURI)
 	if token:
 		sp = spotipy.Spotify(auth=token)
 	else:
@@ -124,7 +130,7 @@ def initialize_spotify():
 
 def main():
 	global songList
-	loop_back_through_data()
+	loop_back_through_data(1)
 	get_song_URI()
 	for i in range(0, len(songList)):
 		songList[i].print_name()
@@ -136,7 +142,6 @@ def main():
 	get_spotify_details()
 	for i in range(0, len(songList)):
 		songList[i].print_features()
-	#numpy.savetxt("test.txt", str(songList), newline = " ")
 
 
 if __name__ == "__main__": main()
