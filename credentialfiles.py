803b3c7df8706e361d13ca89b1c573bad77a8d82
semcmillan <semcmill@ncsu.edu>
2018-03-23 10:01:20 -0400

    Delete credentialfiles.py

--- credentialfiles.py
+++
@@ -1,7 +0,0 @@
-class Credentials:
-	ClientID 		= 'cdf75d8320f640d09cc179c34d18e803'
-	ClientSecret 	= 'f9be99648e40467f8c52a3bc7ef2d65a'
-	RedirectURI 	= 'http://localhost:/callback'
-	UserID 			= '1224319843'
-	username		= 'semcmill'
-	scope 			= 'user-library-read'
